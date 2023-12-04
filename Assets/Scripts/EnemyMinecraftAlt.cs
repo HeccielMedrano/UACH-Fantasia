fda05b5470518e2bbb96d03d2a92ec06891e5e6e
HeccielMedrano <146401143+HeccielMedrano@users.noreply.github.com>
2023-12-04 01:35:22 -0600

    Extended Level 1

---
+++ Assets/Scripts/EnemyMinecraftAlt.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMinecartAlt : MonoBehaviour
+{
+    bool onRails;
+    private BoxCollider2D coll;
+    public Vector3 colliderOffset = new Vector3(0.1f, 0.18f, 0f);
+    float railLength = 2f;
+    public LayerMask railLayer;
+    public float moveSpeed = 0.02f;
+
+    public GameObject projectilePrefab;
+
+    public float projectileInterval;
+
+    public AudioSource audio;
+    public AudioClip clip;
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnProjectilesRepeatedly());
+    }
+
+    IEnumerator SpawnProjectilesRepeatedly()
+    {
+        while (true)
+        {
+            SpawnProjectile();
+
+            yield return new WaitForSeconds(projectileInterval);
+        }
+    }
+
+    void SpawnProjectile()
+    {
+        audio.clip = clip;
+        audio.Play();
+        
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+        projectile.GetComponent<SpriteRenderer>().sortingOrder = 999;
+
+        Destroy(projectile, 4f);
+    }
+
+
+    void Update()
+    {
+        onRails = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, railLength, railLayer);
+
+        if (onRails)
+        {
+            transform.position = new Vector2(transform.position.x + moveSpeed, transform.position.y);
+        }
+        else
+        {
+            flipMinecart();
+            moveSpeed *= -1;
+        }
+    }
+
+    public void flipMinecart()
+    {
+        colliderOffset = new Vector3(colliderOffset.x * -1, 0.18f, 0f);
+    }
+}
