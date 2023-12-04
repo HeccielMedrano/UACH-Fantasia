6e798135a29a1ba526b08113577d6a7543036d07
juanomanzano <ebaeza05@gmail.com>
2023-12-04 03:21:01 -0700

    Auto stash before merge of "main" and "origin/main"
    fondo nivel 3 y eso ya me estoy muriendo

---
+++ Assets/Scripts/Fondo2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fondo2 : MonoBehaviour
+{
+    [SerializeField] private Vector2 velocidadMovimiento;
+    private Vector2 offset;
+    private Material material;
+    private Rigidbody2D jugadorRB;
+
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+        jugadorRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = (jugadorRB.velocity.y * 0.1f) * velocidadMovimiento * Time.deltaTime;
+        material.mainTextureOffset += offset;
+    }
+}
